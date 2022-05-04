50f1ef89251cc59db5372f8be863012cefe96514
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-05-04 01:52:21 -0300

    feat: add part of payment validation

--- Back-end/src/utils/paymentValidation.js
+++ Back-end/src/utils/paymentValidation.js
@@ -1,9 +1,29 @@
 const Joi = require('joi');
 const errorHandler = require('../utils/errorHandler');
 const { badRequest } = require('../utils/statusCode');
-const { Payment } = require('../database/models');
+const { Payment, People } = require('../database/models');
 
 const schema = Joi.object({
   value: Joi.number().required(),
   peopleId: Joi.number().required(),
 });
+
+const validatePayment = async (paymentData) => {
+  const { error } = schema.validate(paymentData);
+  if (error) throw errorHandler(badRequest, error.message);
+
+  const people = await People.findByPk(paymentData.peopleId);
+  if (!people)
+    throw errorHandler(
+      badRequest,
+      'Não é possivel registrar pagamento para pessoa não cadastrada',
+    );
+
+  const {
+    dataValues: { payments },
+  } = people;
+};
+
+module.exports = {
+  validatePayment,
+};
