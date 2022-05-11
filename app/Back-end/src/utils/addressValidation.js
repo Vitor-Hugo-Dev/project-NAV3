f7f033ceccd21c53d9f533bc62075705969b8914
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-05-11 19:24:20 -0300

    refatoring:  change the local of validation

--- app/Back-end/src/utils/addressValidation.js
+++ app/Back-end/src/utils/addressValidation.js
@@ -1,16 +1,18 @@
 const Joi = require('joi');
+const errorHandler = require('./errorHandler');
+const { badRequest } = require('./statusCode');
 
-const validateAddress = (addressData) => {
-  const scheema = Joi.object({
-    district: Joi.string().required(),
-    street: Joi.string().required(),
-    number: Joi.number().required(),
-    complement: Joi.string().min(10).required(),
-    peopleId: Joi.number().required(),
-  });
-  return scheema.validate(addressData);
-};
+const scheema = Joi.object({
+  district: Joi.string().required(),
+  street: Joi.string().required(),
+  number: Joi.number().required(),
+  complement: Joi.string().min(10).required(),
+  peopleId: Joi.number().required(),
+});
 
 module.exports = {
-  validateAddress,
+  validateAddress: (addressData) => {
+    const { error } = scheema.validate(addressData);
+    if (error) throw errorHandler(badRequest, error.message);
+  },
 };
