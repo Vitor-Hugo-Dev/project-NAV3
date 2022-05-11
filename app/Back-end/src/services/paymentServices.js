1c7a55a03466d175769e407e066b5250f0249876
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-05-11 20:00:37 -0300

    feat: add a transaction

--- app/Back-end/src/services/paymentServices.js
+++ app/Back-end/src/services/paymentServices.js
@@ -3,18 +3,30 @@ const errorHandler = require('../utils/errorHandler');
 const { notFound } = require('../utils/statusCode');
 const { validatePayment } = require('../utils/paymentValidation');
 const { updatePeopleService } = require('./peopleServices');
+const Sequelize = require('sequelize');
+const config = require('../database/config/config');
+const sequelize = new Sequelize(config.development);
 
 module.exports = {
   registerPaymentService: async (paymentData) => {
+    const transaction = {
+      transaction: await sequelize.transaction(),
+    }; // inicia uma transação
     try {
       await validatePayment(paymentData);
-      const newPayment = await Payment.create(paymentData);
+      const newPayment = await Payment.create(paymentData, transaction);
 
-      await updatePeopleService(paymentData.peopleId, {
-        paymentMonth: newPayment.dataValues.paymentMonth,
-      });
+      await updatePeopleService(
+        paymentData.peopleId,
+        {
+          paymentMonth: newPayment.dataValues.paymentMonth,
+        },
+        transaction,
+      );
+      await transaction.transaction.commit(); // commita a transação
       return newPayment;
     } catch (error) {
+      await transaction.transaction.rollback(); // rollbacka a transação
       throw errorHandler(notFound, error.message);
     }
   },
