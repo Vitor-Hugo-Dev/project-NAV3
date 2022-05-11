8e671035a3b7208c995d92c4186df2ecbb665d5e
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-05-11 18:46:13 -0300

    fix: fix tableName error in model

--- app/Back-end/src/database/models/contactInfos.js
+++ app/Back-end/src/database/models/contactInfos.js
@@ -1,27 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
-  const ContactInfos = sequelize.define('ContactInfos', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const ContactInfos = sequelize.define(
+    'ContactInfos',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      peopleId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        foregnKey: true,
+        field: 'peopleId',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    peopleId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      foregnKey: true,
-      field: 'peopleId',
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
+    {
+      timestamps: false,
+      tableName: 'contactInfos',
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phoneNumber: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+  );
   ContactInfos.associate = (models) => {
     ContactInfos.belongsTo(models.People, {
       foreignKey: 'peopleId',
